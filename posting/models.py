513b53f72a82b2304982d24d31ddc8d80e83bd03
Dev-Aligator <devaligator1004@gmail.com>
2023-05-02 16:40:27 +0700

    Add Google Drive URL handling to image_url field

--- posting/models.py
+++ posting/models.py
@@ -15,5 +15,21 @@ class Post(models.Model):
     most = models.BooleanField(default=False, verbose_name="Display at the Most picked Panel?")
     popular = models.BooleanField(default=False, verbose_name="Display at the Polular Panel?")
 
+    def save(self, *args, **kwargs):
+        if self.image_url:
+            if 'drive.google.com' in self.image_url:
+                # extract the ID from the original URL
+                id_index = self.image_url.find('/d/') + 3
+                view_index = self.image_url.find('/view')
+                file_id = self.image_url[id_index:view_index]
+
+                # create the new URL with the ID
+                new_url = 'https://drive.google.com/uc?export=view&id=' + file_id
+
+                # set the image_url field to the new URL
+                self.image_url = new_url
+
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
