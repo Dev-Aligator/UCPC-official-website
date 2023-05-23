90ab8d4d992fc6fffc2e15270904b30c87f6e660
Dev-Aligator <devaligator1004@gmail.com>
2023-05-23 18:34:20 +0700

    Fix auto redirect url

--- ucpc/urls.py
+++ ucpc/urls.py
@@ -38,7 +38,6 @@ if settings.SOCIALACCOUNT_ENABLED:
     urlpatterns += [path("social/", include("allauth.socialaccount.urls"))]
 
 
-# Provider urlpatterns, as separate attribute (for reusability).
 provider_urlpatterns = []
 for provider in providers.registry.get_list():
     try:
@@ -47,5 +46,6 @@ for provider in providers.registry.get_list():
         continue
     prov_urlpatterns = getattr(prov_mod, "urlpatterns", None)
     if prov_urlpatterns:
+        prov_urlpatterns = [path("info/" + str(url.pattern), include((url.url_patterns, url.namespace))) for url in prov_urlpatterns]
         provider_urlpatterns += prov_urlpatterns
 urlpatterns += provider_urlpatterns
